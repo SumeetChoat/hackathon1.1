aceecf008411afdf4bbb1f5966e0958c7b04241b
“SumeetChoat” <sumeetpalchoat@hotmail.co.uk>
2023-06-09 11:47:48 +0100

    completed game by adding switch case for computer choice and rules for how to determine wiiner

--- hackathon1.1.js
+++ hackathon1.1.js
@@ -20,7 +20,7 @@ const getUserChoice = (userInput) => {
 //2) get computer choice(funny because they can't think it's just randomised)
 
 const getComputerChoice = () => {
-    let randNum = math.random()*3;
+    let randNum = Math.floor(Math.random()*3);
     return randNum === 0 ?
     'rock' : (randNum === 1 ?
     'paper' : 'scissors');
@@ -41,3 +41,46 @@ const getComputerChoice = () => {
 };
 
 console.log(getComputerChoice());
+
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return 'tie game'
+    } else 
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'The computer won';
+        } else {
+            return 'you win'
+        }
+    }
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'The computer wins';
+        } else {
+            return 'you won!'
+        }
+    }
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'the computer won!';
+        } else {
+            return 'you won'
+        }
+    }
+}
+
+console.log(determineWinner('rock', 'paper'));
+console.log(determineWinner('paper', 'scissors'));
+console.log(determineWinner('paper', 'paper'));
+console.log(determineWinner('paper', 'rock'));
+
+const playGame = () => {
+    const userChoice = getUserChoice('scissors');
+    const computerChoice = getComputerChoice();
+        console.log('you threw: ' + userChoice);
+        console.log('the computer threw: ' + computerChoice);
+        console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
